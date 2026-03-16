c89ffef39b46ebf08ef562bd3f3d487fbe36972e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-16 14:00:30 +0530

    new files

---
+++ Basic Python Functions/max-of-2-No.py
@@ -0,0 +1,15 @@
+def max(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+maximum = max(345, 7635)
+print(maximum)
+
+# Important Concept 🔑
+# print() → shows output on screen
+# return → sends value back to the caller
+# if I use print function the o/p prints max number and none becuse while checking with value 
+# for b there is no value greater making maximum = none hence 7735, none. To solve this use 
+# return
