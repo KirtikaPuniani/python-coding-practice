ec5cd8e13e47d333673e8d337c8d826316c8b04b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-16 14:08:38 +0530

    factorial

---
+++ Basic Python Functions/factorial.py
@@ -0,0 +1,19 @@
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact = fact * i
+#     return fact
+
+# x = factorial(5)
+# print(x)
+
+#using recursion
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        N = n * factorial(n-1)
+        return N
+    
+x = factorial(5)
+print(x)
