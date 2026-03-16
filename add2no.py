ebba791bacdcb04ebaad0db5602a1e9b1d7b8acd
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-16 13:50:44 +0530

    first code

---
+++ add2no.py
@@ -0,0 +1,12 @@
+# a = int(input("please give the first number: "))
+# b = int(input("please give the second number: "))
+
+# c = a+b
+# print(c)
+
+
+def add(a,b):
+    return a+b
+
+sum = add(46568,67434)
+print(sum)
