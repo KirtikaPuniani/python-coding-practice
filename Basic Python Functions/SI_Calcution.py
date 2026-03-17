7254bec64694ebba2acbab71a51488a5621dcbac
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-17 17:47:58 +0530

    new file:   Basic Python Functions/SI_Calcution.py

---
+++ Basic Python Functions/SI_Calcution.py
@@ -0,0 +1,7 @@
+# principal = int(input("please enter the initial amount: "))
+# rate = int(input("please enter the rate of interest per annum: "))
+# time = int(input("please enter the time period in years: "))
+
+# Simple_Interest = (principal * rate * time) / 100
+# print(Simple_Interest)
+
