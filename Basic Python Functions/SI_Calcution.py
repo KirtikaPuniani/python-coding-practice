7d8bf2da9201b84e0f07144976d94610db8ca137
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-17 17:51:17 +0530

    new method

--- Basic Python Functions/SI_Calcution.py
+++ Basic Python Functions/SI_Calcution.py
@@ -5,10 +5,16 @@
 # Simple_Interest = (principal * rate * time) / 100
 # print(Simple_Interest)
 
-#using function
-def simple_interest(p,r,t):
-    SI = (p * r * t) / 100
-    return SI
+# #using function
+# def simple_interest(p,r,t):
+#     SI = (p * r * t) / 100
+#     return SI
 
-SI = simple_interest(10000,3,1)
+# SI = simple_interest(10000,3,1)
+# print(SI)
+
+#using lambda
+Simple_Interest = lambda p,r,t: (p*r*t)/100
+p,r,t = 10000, 3, 1
+SI = Simple_Interest(p,r,t)
 print(SI)
