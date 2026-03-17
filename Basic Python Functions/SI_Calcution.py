6677e97a73bc0499e4649863edb276a3794ee17e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-17 17:52:03 +0530

    updated file

--- Basic Python Functions/SI_Calcution.py
+++ Basic Python Functions/SI_Calcution.py
@@ -1,17 +1,17 @@
-# principal = int(input("please enter the initial amount: "))
-# rate = int(input("please enter the rate of interest per annum: "))
-# time = int(input("please enter the time period in years: "))
+principal = int(input("please enter the initial amount: "))
+rate = int(input("please enter the rate of interest per annum: "))
+time = int(input("please enter the time period in years: "))
 
-# Simple_Interest = (principal * rate * time) / 100
-# print(Simple_Interest)
+Simple_Interest = (principal * rate * time) / 100
+print(Simple_Interest)
 
-# #using function
-# def simple_interest(p,r,t):
-#     SI = (p * r * t) / 100
-#     return SI
+#using function
+def simple_interest(p,r,t):
+    SI = (p * r * t) / 100
+    return SI
 
-# SI = simple_interest(10000,3,1)
-# print(SI)
+SI = simple_interest(10000,3,1)
+print(SI)
 
 #using lambda
 Simple_Interest = lambda p,r,t: (p*r*t)/100
