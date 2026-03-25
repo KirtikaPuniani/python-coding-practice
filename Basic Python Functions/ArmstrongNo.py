5775421a6009bc05ac7050d025cee241a96777b6
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-03-25 18:24:35 +0530

    armstrong number

--- Basic Python Functions/ArmstrongNo.py
+++ Basic Python Functions/ArmstrongNo.py
@@ -1,15 +1,30 @@
-class ArmstrongNumber(object):
-    def armstrong(self, x):
-        n = len(str(x))
-        temp = x
-        total = 0
+# class ArmstrongNumber(object):
+#     def armstrong(self, x):
+#         n = len(str(x))
+#         temp = x
+#         total = 0
 
-        while x > 0:
-            digit = x % 10
-            total += digit ** n
-            x //= 10
+#         while x > 0:
+#             digit = x % 10
+#             total += digit ** n
+#             x //= 10
 
-        return total == temp
+#         return total == temp
 
-X = ArmstrongNumber()
-print(X.armstrong(1634))
+# X = ArmstrongNumber()
+# print(X.armstrong(1634))
+
+num = int(input("Enter a number: "))
+n = num
+power = len(str(num))
+total = 0
+
+while n > 0:
+    digit = n % 10
+    total += digit ** power
+    n //= 10
+
+if total == num:
+    print("It is an Armstrong Number")
+else:
+    print("It is not an Armstrong Number")
