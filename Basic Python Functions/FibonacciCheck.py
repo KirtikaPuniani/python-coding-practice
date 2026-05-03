9679b7d5dbbe31567980ba69b3dd280e1fdb31b3
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-05-04 01:48:52 +0530

    Check if a Given Number is Fibonacci number

--- Basic Python Functions/FibonacciCheck.py
+++ Basic Python Functions/FibonacciCheck.py
@@ -1,3 +1,4 @@
+#Using Loop
 def fib_check(n):
     x, y = 0, 1
     while y < n:
@@ -7,3 +8,18 @@ def fib_check(n):
 
 a = fib_check(10) 
 print(a)
+
+
+
+#Using math -- interview favourite
+import math
+
+def is_perfect_square(n):
+    x = int(math.sqrt(n))
+    return x * x == n
+
+def fib_check(y):
+    return is_perfect_square(5*y*y + 4) or is_perfect_square(5*y*y - 4)
+
+b = fib_check(47)
+print(b)
