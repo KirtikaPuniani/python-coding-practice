e80e195fe830824c6aa52f13919967f2971143dc
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-05-04 01:33:59 +0530

    updated file

--- Basic Python Functions/N-th_Fibonacci_No.py
+++ Basic Python Functions/N-th_Fibonacci_No.py
@@ -10,6 +10,7 @@ print(x)
 print(y)
 
 
+#--------------------------------------------------------------
 
 
 #Using Dynamic Programing for space optimization
@@ -23,3 +24,28 @@ def fibonacci(n):
 
 f = fibonacci(10)
 print(f)
+
+# a, b = 0, 1 stores first two values
+# for _ in range(n) runs loop n times
+# a, b = b, a + b shifts values to next pair
+# return a returns required Fibonacci number
+
+
+
+#--------------------------------------------------------------
+
+
+# Recursion with Memoization
+# Memoization method stores previously computed Fibonacci values in a dictionary. When a value is needed again, it is directly returned instead of recalculating.
+
+fib_cache = {0: 0, 1: 1}
+
+def fibonacci(n):
+    if n in fib_cache:
+        return fib_cache[n]
+    else:
+        fib_cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return fib_cache[n]
+
+a = fibonacci(12)
+print(a)
