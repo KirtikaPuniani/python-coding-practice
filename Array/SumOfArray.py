c02810ee651895030f9b13572b43621809afa1cc
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-05-20 13:34:02 +0530

    Sum of elements in array

--- Array/SumOfArray.py
+++ Array/SumOfArray.py
@@ -7,3 +7,22 @@ def sumOfArray(arr):
 
 x = sumOfArray(arr)
 print(x)
+
+
+#using in-built reduce() funtion: this function from functools applies a
+#function cumulatively to the elements of an iterable, effectively 
+#summing all elements
+
+from functools import reduce
+arr = [1, 2, 3, 4, 5, 6, 7]
+result = reduce(lambda a , b: a +b, arr)
+print('Sum of the elements =', result)
+
+#Using enumerate() function: this function allows loopinf through an 
+#array with an index and element
+
+arr = [1, 2, 3, 4, 5]
+sum = 0
+for i, val in enumerate(arr):
+    sum += val
+print(sum)
