e163c8ad8ffee264d5f6d39906ccb9e85ae3e0df
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-05-22 11:30:11 +0530

    Program for reversal algorithm for array rotation

--- Array/ArrayReversal.py
+++ Array/ArrayReversal.py
@@ -1,2 +1,15 @@
 #Reversal algorithm for array rotation
 
+arr = [1, 2, 3, 4, 5, 6, 7]
+d = 3
+n = len(arr)
+
+#reverse first d element
+arr[:d] = reversed(arr[:d])
+
+#reverse remaining elements
+arr[d:] = reversed(arr[d:])
+
+#reversing the entire array
+arr.reverse()
+print(arr)
