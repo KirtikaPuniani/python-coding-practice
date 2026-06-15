571dfadfe77f255b7c4dcacd2d1068d3af4a2ed3
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-06-15 16:03:01 +0530

    Sum of Elements in an Array

--- Array/SumOfElements.py
+++ Array/SumOfElements.py
@@ -6,3 +6,11 @@ def SumOfElements(arr):
     return sum
 x = SumOfElements(arr)
 print("Sum of elements in the list:", x)
+
+
+
+#Using lambda function
+from functools import reduce    
+arr = [1, 2, 3, 4, 5]
+sum = reduce(lambda x, y: x+y, arr)
+print("Sum of elements in the list:", sum)
