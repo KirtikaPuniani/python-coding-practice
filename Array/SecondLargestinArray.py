ceb7107fd4d89e756b81dd78f316f6475aeac0e6
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-06-17 13:51:34 +0530

    Second Largest in Array

--- Array/SecondLargestinArray.py
+++ Array/SecondLargestinArray.py
@@ -17,3 +17,12 @@ def secondLargest(arr):
 
 x = secondLargest(arr)
 print("Second largest element in the list:", x)
+
+
+
+
+#Using heapq.largest() function to find the second largest element in the list
+import heapq
+arr = [1, 15, 34, 25, 78, 19, 20, 87, 100, 89, 200]
+second_largest = heapq.nlargest(2, arr)
+print(second_largest[1])  # Output: 100
