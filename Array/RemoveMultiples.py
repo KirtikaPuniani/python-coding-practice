85707c6ec2f2b16575db08c8a7f29ce825d6e859
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-06-22 13:59:29 +0530

    remove multiples elements from a list

--- Array/RemoveMultiples.py
+++ Array/RemoveMultiples.py
@@ -1,11 +1,11 @@
 #Remove multiples elements from a list
 
 arr = [1,2,3,4,5,6,7,8,9,10]
+remove = [2,4,6,7,10]
 def removeMultiples(arr):
-    new_arr = []
-    for a in arr:
-        if a % 2 != 0:
-            new_arr.append(a)
-    return new_arr
+    for a in remove:
+        if a in arr:
+            arr.remove(a)
+    return arr
 
 print(removeMultiples(arr))
