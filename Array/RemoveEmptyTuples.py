6c2288ffc43dd9d38e783272f244c483ebef03d6
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-06-29 22:18:29 +0530

    Remove empty truples from a list

--- Array/RemoveEmptyTuples.py
+++ Array/RemoveEmptyTuples.py
@@ -1,15 +1,15 @@
 #Remove empty tuples from a list
 
-arr = [(1,2,3,4),(),(5,6),(),(7,8,9),(),(10)]
-result = []
+# arr = [(1,2,3,4),(),(5,6),(),(7,8,9),(),(10)]
+# result = []
 
-def removemptyTuples():
-    for a in arr:
-        if a:
-            result.append(result)
-    return result
+# def removemptyTuples():
+#     for a in arr:
+#         if a:
+#             result.append(result)
+#     return result
 
-print(removemptyTuples())
+# print(removemptyTuples())
 
 
 
