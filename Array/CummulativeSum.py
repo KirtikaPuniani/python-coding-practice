79a62ce079b6e3b0794cf709133f262013cd03ad
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-02 15:54:05 +0530

    Find cumulative sum of a list

--- Array/CummulativeSum.py
+++ Array/CummulativeSum.py
@@ -3,9 +3,14 @@
 #the task is to find the cumulative sum (also known as the running total) where each element in the output represents the sum of all elements up to that 
 #position in the original list.
 
+import itertools
 arr = [2,3,5,7,9,18,45]
+result = list(itertools.accumulate(arr))
+print(result)
 
-def cummulativeSum(arr):
-    sum = 0
-    for a in arr:
+# arr = [2,3,5,7,9,18,45]
+
+# def cummulativeSum(arr):
+#     sum = 0
+#     for a in arr:
         
