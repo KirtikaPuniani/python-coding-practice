2c5947d9e547cf74957ed501ee5d74f63e55ce5f
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-03 16:02:53 +0530

    Sum of number of digits in list

--- Array/SumNumDigits.py
+++ Array/SumNumDigits.py
@@ -1,5 +1,13 @@
 #Sum of number of digits in list
 
+# Given a list of integers, write a Python program to calculate the sum of digits for each element and store the results in a new list. 
+# For Example:
+# Input: [123, 456, 789]  
+# Output: [6, 15, 24]
+# Explanation:  123 = 1 + 2 + 3 = 6  
+#               456 = 4 + 5 + 6 = 15  
+#               789 = 7 + 8 + 9 = 24 
+
 arr = [12, 13, 14, 15, 16, 17, 18, 19]
 total = [sum(int(digit) for digit in str(num)) for num in arr]
 print(total)
