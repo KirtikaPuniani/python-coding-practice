a6ccd5bc17bef65158ef2ddf0ec3ca0cd07435bd
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-03 16:02:07 +0530

    Sum of number of digits in list

--- Array/SumNumDigits.py
+++ Array/SumNumDigits.py
@@ -1,5 +1,5 @@
 #Sum of number of digits in list
 
 arr = [12, 13, 14, 15, 16, 17, 18, 19]
-total = sum(len(str(num)) for num in arr)
+total = [sum(int(digit) for digit in str(num)) for num in arr]
 print(total)
