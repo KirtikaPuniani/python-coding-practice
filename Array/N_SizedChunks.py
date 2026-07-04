af602b14e6dc0c09cf3fd93086c011a7851c7d32
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-04 18:05:19 +0530

    deleting a file

--- Array/N_SizedChunks.py
+++
@@ -1,8 +0,0 @@
-#Break a List into Chunks of Size N in Python
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8]
-
-#Using List Comprehension
-n = 3
-result = [arr[i:i +n] for i in range(0, len(arr), n)]
-print(result)
