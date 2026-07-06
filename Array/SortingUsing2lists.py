15210485eceb298e6188840043727b929ec2b5d8
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-06 14:59:37 +0530

    updatedn code

--- Array/SortingUsing2lists.py
+++ Array/SortingUsing2lists.py
@@ -1,5 +1,8 @@
 #Sort the values of first list using second list in Python
 
+#Given two lists of equal length, where the second list defines the order, the task is to reorder the first list according to the sorted order 
+#of the second list.
+
 list1 = ['a', 'c', 'b', 'd', 'e']
 list2 = [10, 40, 20, 30, 50]
 
