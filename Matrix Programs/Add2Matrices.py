a6d90c01e107d32a218ce6baa9ca46769073dbdb
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-09 15:32:45 +0530

    Add two matrices

---
+++ Matrix Programs/Add2Matrices.py
@@ -0,0 +1,12 @@
+# The task of adding two matrices in Python involves combining corresponding elements from two given matrices to produce a new matrix. 
+# Each element in the resulting matrix is obtained by adding the values at the same position in the input matrices.
+
+
+
+# Using Numpy
+
+import numpy as np
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[7, 8, 9], [10, 11, 12]])
+result = a + b
+print(result)
