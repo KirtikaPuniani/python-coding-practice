99e7f17abcfcf1a231da4ced4712c7616121e2de
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-13 14:02:33 +0530

    multiply two matrices

--- Matrix Programs/Multiply2Matrices.py
+++ Matrix Programs/Multiply2Matrices.py
@@ -15,3 +15,23 @@ for row in result:
 #Shape of a is (2, 3) (2 rows, 3 columns) and Shape of b is also (2, 3) and for matrix multiplication, the number of columns in the first matrix 
 #must be equal to the number of rows in the second matrix. whereas in this case, a : (2 × 3) and b : (2 × 3), 3 ≠ 2; So we need to transpose b to 
 #make it (3, 2) so that the multiplication can be performed correctly.
+
+
+
+
+#Using nested loops
+a = np.array([[1, 2, 3], 
+      [4, 5, 6]])
+
+b = np.array([[7, 8, 9], 
+      [10, 11, 12]])
+
+result = [[0] * len(b[0]) for _ in range(len(a))]
+
+for i in range(len(a)):
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            result[i][j] += a[i][k] * b[k][j]
+
+for row in result:
+    print(row)
