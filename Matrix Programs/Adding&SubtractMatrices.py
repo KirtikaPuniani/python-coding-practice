dd4dd719cf8fb258609fabe73efea26cd07003d5
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-14 11:52:16 +0530

    Adding and Subtracting two matrices

--- Matrix Programs/Adding&SubtractMatrices.py
+++ Matrix Programs/Adding&SubtractMatrices.py
@@ -10,3 +10,20 @@ print("result of addition: \n", c)
 
 d = np.subtract(a,b)
 print("result of subtraction: \n", d)
+
+
+
+#Using nested loops
+
+a = np.array([[1,2], [3,4]])
+b = np.array([[5,6], [7,8]])
+c = [[0,0], [0,0]]
+d = [[0,0], [0,0]]
+
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        c[i][j] = a[i][j] + b[i][j]
+        d[i][j] = a[i][j] - b[i][j]
+
+print("result of addition: \n", c)
+print("result of subtraction: \n", d)
