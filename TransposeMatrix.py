3a31dacfa2d4e2e188d005db634866ee50cbcff2
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-14 13:16:31 +0530

    Transpose a matrix in single line

--- TransposeMatrix.py
+++ TransposeMatrix.py
@@ -24,3 +24,25 @@ for row in transposed:
 import numpy as np
 matrix = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
 print(np.transpose(matrix))
+
+
+
+
+#Using Itertools
+from itertools import chain
+import time
+import numpy as np
+def transpose(matrix):
+    matrix = matrix.tolist()
+    n = len(matrix[0])
+    l = list(chain(*matrix))
+    return [l[i::n] for i in range(n)]
+
+m = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
+
+start = time.time_ns()
+result = transpose(matrix)
+end = time.time_ns()
+
+print(result)
+print("Time taken to transpose the matrix using itertools: ", end-start, "ns")
