d88281976ad2d58c30283690d426c2502adf569f
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-14 13:13:42 +0530

    Transpose a matrix in single line

--- TransposeMatrix.py
+++ TransposeMatrix.py
@@ -17,3 +17,10 @@ matrix = [(1,2,3), (4,5,6),
 transposed = zip(*matrix)
 for row in transposed:
     print(row)
+    
+    
+
+#Using numpy
+import numpy as np
+matrix = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
+print(np.transpose(matrix))
