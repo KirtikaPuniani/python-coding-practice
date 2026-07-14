065681c57d5a4d488961c7c2f2fed1d043bc3309
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-14 15:14:49 +0530

    Given a number n, create an n × n matrix

---
+++ Matrix Programs/N*NMatrix.py
@@ -0,0 +1,42 @@
+#Given a number n, the task is to create an n × n matrix in Python.
+
+#Using Numpy zeros
+
+import numpy as np
+n = 3
+result = np.zeros((n,n), dtype=int)
+for row in result:
+    print(row)
+    
+    
+    
+    
+#Using numpy full()
+import numpy as np
+n = 3
+result = np.full((n,n), 5, dtype=int)
+for row in result:
+    print(row)
+    
+
+
+
+#Using Nested list comprehension
+n = 3
+result = [[0 for j in range(n)] for i in range(n)]
+for row in result:
+    print(row)
+    
+    
+
+#Using nested loops
+n = 3
+m = []
+count = 1
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(count)
+        count += 1
+    m.append(row)
+print(m)
