33db63fe7e4a203f300a02757cd5980e666d4275
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-16 00:36:10 +0530

    Extract the Kth column from a matrix

---
+++ Matrix Programs/KthColumn.py
@@ -0,0 +1,10 @@
+#Extract the Kth column from a matrix
+
+#Using numpy
+import numpy as np
+matrix = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+k = 1  # Extract the 2nd column (0-indexed)
+column = matrix[:, k]
+print(column)
