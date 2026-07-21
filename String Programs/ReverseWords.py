5eedd2089b9c8a60f8b5149057ee3b1e62a6da13
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-21 17:12:47 +0530

    reverse words in a string

--- String Programs/ReverseWords.py
+++ String Programs/ReverseWords.py
@@ -6,3 +6,8 @@
 string = "Learning python is easy"
 result = ' '.join(string.split()[::-1])
 print(result)
+#explanation: 
+#string.split() splits the string into the list of words
+#[::-1] reverses the list of words
+#''.join() concatinates the reversed words into a single string separated by spaces
+
