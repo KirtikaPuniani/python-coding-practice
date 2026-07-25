7621fba1f8c26bd2a2e7ae4355af73af539aba2e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-25 18:04:12 +0530

    Remove letters from a string

---
+++ String Programs/RemoveLetter.py
@@ -0,0 +1,9 @@
+#Remove letters from a string
+
+
+#Using replace
+string = "hello world"
+string = string.replace("l", "")     #replaces every "l" with an empty string "" or anything specified after the letter that needs to be replaced
+print(string)      #and replacement creates a new string and the original string remains unchanged
+
+
