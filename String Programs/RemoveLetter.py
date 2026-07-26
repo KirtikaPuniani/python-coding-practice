5754c188f4aee3f7b9d20b943b9f63502e39acbb
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-26 23:01:25 +0530

    Remove letters from a string

--- String Programs/RemoveLetter.py
+++ String Programs/RemoveLetter.py
@@ -7,3 +7,8 @@ string = string.replace("l", "")     #replaces every "l" with an empty string ""
 print(string)      #and replacement creates a new string and the original string remains unchanged
 
 
+#Using filter function
+#filter function provides an efficient way to filter out characters baces on a condition. It returns an iterator which can be converted back to a string
+string = "hello world"
+string = "".join(filter(lambda c: c != "o", string))  #filter checks evry charac ter in string and removes 'o' and join merges the remaining char into single string
+print(string)
