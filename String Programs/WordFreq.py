473c3e72f27a5fe124f50d834bec5688ce11d277
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-28 15:02:59 +0530

    Words Frequency in String Shorthands

--- String Programs/WordFreq.py
+++ String Programs/WordFreq.py
@@ -1,1 +1,9 @@
 #Words Frequency in String Shorthands
+#given a string the task is to find how many times each word appears in it.
+
+
+#Using collections.counter
+from collections import Counter
+string = 'hello how is everyone this fine morning'
+result = Counter(string.split())
+print(result)
