fa88eef34fc2d6c3aab66c5fa8c03b5289b31334
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-28 14:05:38 +0530

    check if the string contains substring

---
+++ String Programs/SubStringCheck.py
@@ -0,0 +1,8 @@
+#check if the string contains substring
+
+#given two strings check whether a substring is in the given string
+#example: string1 = 'iamhappytoday', string2 = 'am', O/P = yes
+#example: string1 = 'iamhappytoday', string2 = 'for', O/P = no
+
+
+#using in
