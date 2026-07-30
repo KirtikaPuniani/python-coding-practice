56d44bff31951429968504f25f3b72c51d267ac2
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-30 14:34:37 +0530

    Words Frequency in String Shorthands

--- String Programs/WordFreq.py
+++ String Programs/WordFreq.py
@@ -5,5 +5,5 @@
 #Using collections.counter
 from collections import Counter
 string = 'hello how is everyone this fine morning'
-result = Counter(string.split())
+result = Counter(string.split())    #counter automatically counts how many rimes each word appears in a listwhen we split the string into words.
 print(result)
