a8fa86f24cb154c154fdabd43a906d7c59139aad
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-31 14:25:17 +0530

    Convert Snake case to Pascal case

---
+++ String Programs/SnakeToPascal.py
@@ -0,0 +1,7 @@
+#Convert Snake case to Pascal case
+#Given a string in snake_case, the task is to convert in into PascalCase here each word dtarts with an upper case and there are no underscores.
+#Eg: I/P: hello_everyone_iam_happy and O/P: HelloEveryoneIAmHappy
+
+
+
+#Using split and capitalize
