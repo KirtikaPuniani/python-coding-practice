0d013810de0cccba49886ec0f3319c50d25f7e8c
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-31 13:54:15 +0530

    word frequecy

--- String Programs/WordFreq.py
+++ String Programs/WordFreq.py
@@ -7,3 +7,11 @@ from collections import Counter
 string = 'hello how is everyone this fine morning'
 result = Counter(string.split())    #counter automatically counts how many rimes each word appears in a listwhen we split the string into words.
 print(result)
+
+
+#using dict.get() with a fpr loop
+string = 'hello hello everyone'
+result = {}
+for word in string.split():
+    result[word] = result.get(word, 0) + 1
+print(result)
