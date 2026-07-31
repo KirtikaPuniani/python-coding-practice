df76d8e7607c53d15ffb8e9d8d768a02b0a48275
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-07-31 15:21:39 +0530

    find the length of string in python

---
+++ String Programs/lengthOfString.py
@@ -0,0 +1,14 @@
+#find the length of string in python
+
+
+string = 'hello everyone good morning'
+print(len(string))
+
+
+
+#using for loop
+string = 'hello everyone good morning'
+count = 0
+for char in string:
+    count += 1
+print(count)
