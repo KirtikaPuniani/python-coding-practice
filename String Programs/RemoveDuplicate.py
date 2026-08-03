6182e52167535334057ca4fb2524f8eb8ed786a0
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-04 00:11:37 +0530

    remove all duplicates from a string

--- String Programs/RemoveDuplicate.py
+++ String Programs/RemoveDuplicate.py
@@ -2,5 +2,6 @@
 
 #Using dict.fromkeys
 string = 'hello everyone good morning'
-result = ' '.join(dict.fromkeys(string))
+result = ' '.join(dict.fromkeys(string))    #dict.fromkeys() creates a dictionary with characters of s as keys, automatically removing duplicates.
 print(result)
+#Efficient because it scans the string once and preserves order.
