5f5cce1fe8044686748eca09042e69d703e2ee97
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-03 17:31:14 +0530

    Print only the even length words from a string

--- String Programs/EvenLengthWords.py
+++ String Programs/EvenLengthWords.py
@@ -29,3 +29,28 @@ words = string.split()     #splits the string into a list of words
 even_words = (w for w in words if len(w) % 2 == 0)     #creates a generator for words with even length
 result = ' '.join(even_words)      #combines the filtered words back into a string
 print(result)
+
+
+
+
+
+#Using itertools.filterfalse
+from itertools import filterfalse
+string = 'hello everyone good morning'
+words = string.split()     #splits the string into a list of words
+even_words = filterfalse(lambda w: len(w) % 2 != 0, words )     #filters words with even length using filterfalse  
+result = ' '.join(even_words)      #combines the filtered words back into a string
+print(result)
+
+
+
+
+#Using itertools.compress
+from itertools import compress
+string = 'hello everyone good morning'
+words = string.split()     #splits the string into a list of words
+#creates a boolean mask for words with even length
+mask = [len(w) % 2 == 0 for w in words]
+even_words = compress(words, mask)     #filters words with even length using compress and the boolean mask
+result = ' '.join(even_words)      #combines the filtered words back into a string
+print(result)
