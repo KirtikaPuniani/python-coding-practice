3a7369efed99b5479bde14ce7ff993d41980b317
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-03 17:51:18 +0530

    Count the num of matching char in a pair of string

--- String Programs/NumMacthChar.py
+++ String Programs/NumMacthChar.py
@@ -9,3 +9,26 @@ s1 = 'hello'
 s2 = 'world'
 output = len(set(s1.lower()) & set(s2.lower()))     #finds the intersection of both sets and counts the number of common characters
 print(output)
+
+
+
+#Using list comprehension
+s1 = 'apple'
+s2 = 'mango'
+output = len([char for char in set(s1.lower()) if char in s2.lower()])     #creates a list of common characters and counts them
+print(output)
+
+
+
+#Using a for loop
+s1 = 'kiwi'
+s2 = 'grape'
+
+a = s1.lower()
+b = s2.lower()
+count = 0
+
+for char in set(a):
+    if char in b:
+        count += 1
+print(count)
