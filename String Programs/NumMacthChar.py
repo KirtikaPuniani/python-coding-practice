679543393fc2f0b3910b230698a1d1e9bb0a6d4b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-03 17:53:03 +0530

    Count the num of matching char in a pair of string

--- String Programs/NumMacthChar.py
+++ String Programs/NumMacthChar.py
@@ -24,11 +24,11 @@ print(output)
 s1 = 'kiwi'
 s2 = 'grape'
 
-a = s1.lower()
+a = s1.lower()      #convert strings to lowercase for case insensitive comparison
 b = s2.lower()
 count = 0
 
-for char in set(a):
-    if char in b:
-        count += 1
+for char in set(a):      #get unique characters from the first string
+    if char in b:      #checks if each unique character from the first string is present in the second string
+        count += 1      #coungts each matching character
 print(count)
