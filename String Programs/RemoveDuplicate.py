6b23028ede62008bf0dddab2bfd53ed8c281f46c
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-04 13:36:48 +0530

    remove all duplicates from a string

--- String Programs/RemoveDuplicate.py
+++ String Programs/RemoveDuplicate.py
@@ -2,7 +2,7 @@
 
 #Using dict.fromkeys
 string = 'hello everyone good morning'
-result = ' '.join(dict.fromkeys(string))    #dict.fromkeys() creates a dictionary with characters of s as keys, automatically removing duplicates.
+result = ''.join(dict.fromkeys(string))    #dict.fromkeys() creates a dictionary with characters of s as keys, automatically removing duplicates.
 print(result)
 #Efficient because it scans the string once and preserves order.
 
@@ -11,7 +11,7 @@ print(result)
 #OrderedDict works similarly to a diction
 string = 'hello everyone good morning'
 from collections import OrderedDict
-result = ' '.join(OrderedDict.fromkeys(string))    #OrderedDict.fromkeys() creates an ordered dictionary with characters of s as keys, automatically removing duplicates while preserving order.
+result = ''.join(OrderedDict.fromkeys(string))    #OrderedDict.fromkeys() creates an ordered dictionary with characters of s as keys, automatically removing duplicates while preserving order.
 print(result)
 
 
@@ -20,3 +20,15 @@ string = 'hello everyone good morning'
 output = ''.join(set(string))    #set() creates a set of unique characters, but does not preserve order.
 print(output)
 #Less efficient because it scans the string and then creates a set, which may not preserve order
+
+
+
+#Using a for loop with a set
+string = 'hello everyone good morning'
+seen = set()    #set to keep track of seen characters
+result = ""
+for char in string:
+    if char not in seen:    #checks if the character has been seen before
+        seen.add(char)    #adds the character to the seen set
+        result += char    #adds the character to the result string
+print(result)
