e3844242b1e932fe7ed7db905afed62b1ae80dd4
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-08 16:49:35 +0530

    Gen rand string until a given string is generated

--- String Programs/GenRandString.py
+++ String Programs/GenRandString.py
@@ -1,2 +1,27 @@
 #Generate random string until a given string is generated
+#I/P: hello
+#O/P: it will generate random strings until the string "hello" is generated.
 
+
+#Genetic Algorithm
+import string
+import random
+char = string.ascii_letters + string.digits + '.,!?;:'
+target = 'Heyo'
+string = ''.join(random.choice(char) for _ in range(len(target)))
+iterations = 0
+while string != target:
+    print(string)
+    
+    i = random.randint(0, len(string) - 1)
+    l = list(string)
+    l[i] = random.choice(char)
+    string1 = ''.join(l)
+    
+    if sum(a == b for a, b in zip(string1, target)) > sum(a == b for a, b in zip(string, target)):
+        string = string1
+        
+    iterations += 1
+    
+print(string)
+print(f"Target matched after {iterations} iterations")
