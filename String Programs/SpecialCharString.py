f702adf1ec1ef1e4f00af0fb8a0778279acf8d81
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-08 16:40:38 +0530

    Check if a string contains any special character

--- String Programs/SpecialCharString.py
+++ String Programs/SpecialCharString.py
@@ -15,3 +15,39 @@ if __name__ == '__main__':
     string2 = 'He@ll$o W!or/ld'
     run(string)     #calling run fuction
     run(string2)    #calling run function
+    
+    
+    
+#OR#
+string = 'Hello World'
+string.split()
+count = 0
+regex = '[@_!#$%^&*()<>?/\|}{~:]'      #regex pattern to find special characters
+for i in range(len(string)):
+    if string[i] in regex:      #searches the string for special characters
+        count += 1
+if count:
+    print("String rejected")
+else:
+    print("String accepted")
+    
+    
+    
+    
+#Using inbuilt function
+def has_special_char(string):
+    for c in string:
+        if not (c.isalpha() or c.isdigit() or c == ' '):
+            return True
+    return False
+string = 'He@ll$o W!or/ld'
+if has_special_char(string):
+    print("String rejected")
+else:
+    print("String accepted")
+
+string = 'Hello World'
+if has_special_char(string):
+    print("String rejected")
+else:
+    print("String accepted")
