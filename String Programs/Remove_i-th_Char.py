757d369904334e0eda0730a7f9c4261a82c4892e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-11 13:22:39 +0530

    Removing i-th character from a string

--- String Programs/Remove_i-th_Char.py
+++ String Programs/Remove_i-th_Char.py
@@ -1,5 +1,11 @@
-#
+#Removing i-th character from a string
 #Given a string and an index i, remove the character at the i-th position and return the resulting string.
 #Eg: I/P: 'PythonProgramming' and i = 6
 #O/P: 'Pythonrogramming'
 
+
+#Using string slicing
+string = 'PythonProgramming'
+i = 6
+op = string[:i] + string[i+1:]
+print(op)
