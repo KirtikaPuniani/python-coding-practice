dd233bc522f5ce24d7a74974db9fb439e6f6a6ad
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-12 14:33:38 +0530

    Split string into parts based on delimeter & join

--- String Programs/Split&Join.py
+++ String Programs/Split&Join.py
@@ -7,3 +7,17 @@ string = 'Hello, How are you?'
 s = string.split()     #divides the string into a list of words, split by spaces by default
 result = '-'.join(s)     #resembles the list into a string with hyphens between the words
 print(result)
+
+
+
+
+#Using re.split and '-'.join
+#in cases needing advanced splitting, eg, handling multiple spaces or different delimeters re.split from the re module offers more flexibility. However its less efficient than split for 
+#simple cases due to the overhead of regyular expression processing.
+
+import re
+string = 'Hello, How are you?'
+s = re.split(r'\s+', string)     #split by spaces
+a = '-'.join(s)    #join with a hyphen
+# print(s)
+print(a)
