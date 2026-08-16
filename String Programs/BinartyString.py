d7a815478802be54da1d42ade1bb2195d02e1eff
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-16 12:10:02 +0530

    Check if a given string is binary string or not

--- String Programs/BinartyString.py
+++ String Programs/BinartyString.py
@@ -1,6 +1,10 @@
 #Check if a given string is binary string or not python
 
+#A binary string is a string that contains only the character '0' and '1'. eg: '101010' is binary string while '10201' is not. Task is to check if given string is a binary string or not
 
-
-
-
+#Using all
+a = '1010101110001010111000'
+if all(x in '01' for x in a):
+    print("Yes")
+else:
+    print("No")
