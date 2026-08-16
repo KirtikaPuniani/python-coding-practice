2f80d52284b6c2f4bdcdb6fee582d26659a8b67d
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-16 14:44:32 +0530

    Check if a given string is binary string or not

--- String Programs/BinartyString.py
+++ String Programs/BinartyString.py
@@ -24,7 +24,19 @@ else:
 #Using Regular Expression
 import re
 s = '10011000022211110000'
-if re.fullmatch('[01]+', s):
+if re.fullmatch('[01]+', s):      #[01]+ matches one or more occurences of 0 or 1. fullmatch ensures the entire string matches the pattern. regex is poweful concise but slightly slower than all
+    #or set based checks for simple pattern
     print("Yes")
 else:
     print("No")
+    
+    
+
+#Using for loop
+s = '101010203000111'
+for char in s:
+    if char not in '01':
+        print("No")
+        break
+else:
+    print("Yes")
