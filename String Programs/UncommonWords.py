b08c832205657150e48bfd32edce4ee38370129d
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-16 19:15:37 +0530

    Find uncommon words from two strings

--- String Programs/UncommonWords.py
+++ String Programs/UncommonWords.py
@@ -1,1 +1,14 @@
 #Find uncommon words from two strings
+#Extract words that appear in only one of two given strings while ignoring common words.
+#eg: s1 = 'Hi I am here' and s2 = 'Hi I am from London'. O/P = []'from', 'London']
+
+
+
+#Using collections.Counter
+from collections import Counter
+s1 = "Hi I am here"
+s2 = "Hi I am from London"
+
+count = Counter(s1.split()) + Counter(s2.split())
+result = [word for word in count if count[word] == 1]
+print(result)
