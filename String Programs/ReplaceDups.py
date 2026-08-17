ad84e493decccf7a11fed64b8048a263d4864af5
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-17 19:17:34 +0530

    Replace duplicate occurence in string

--- String Programs/ReplaceDups.py
+++ String Programs/ReplaceDups.py
@@ -1,2 +1,4 @@
 #Replace duplicate occurence in string
+#Replace only duplicate occurences of certain words in a string that is, replace a word from its second occurence onwards, while keeping its first occurence unchanged.
+#eg: I/P s = 'Gfg is best. Gfg also has Classes now. Classes help understand better.' O/P: 'Gfg is best. It also has Classes now. They help understand better.'
 
