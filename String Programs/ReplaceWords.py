ef1e76bf6c88de0ce1f36b1b979136e3d0f004c5
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-18 14:02:36 +0530

    Replace multiple words with k

--- String Programs/ReplaceWords.py
+++ String Programs/ReplaceWords.py
@@ -10,3 +10,13 @@ words_to_replace = ['apple', 'banana']
 k = 'kiwi'
 result = ' '.join([k if word in words_to_replace else word for word in s.split()])        #checks each word; if it's in words_to_replace, it replaces it with k
 print(result)
+
+
+
+#Using re.sub() with regex
+import re
+s = 'apple banana oranges'
+words_to_replace = ['apple', 'oranges']
+k = 'strawberry'
+result = re.sub("|".join(sorted(words_to_replace, key = len, reverse = True)), k, s)
+print(result)
