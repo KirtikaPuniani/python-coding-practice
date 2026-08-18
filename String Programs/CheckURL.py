57bff3b120737543628b7aa58d0405e719c41e79
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-18 14:29:05 +0530

    Check for URL in a string

---
+++ String Programs/CheckURL.py
@@ -0,0 +1,6 @@
+#Check for URL in a string
+#We are given a string that may contain one or more URLs and our tast is to extract them efficiently. This is useful for web scraping, text processing and data validation.
+#Eg: Input: s = "My Profile: https://www.geeksforgeeks.org/404.html/ in the portal of https://www.geeksforgeeks.org/"
+#    Output: ['https://www.geeksforgeeks.org/404.html/', 'https://www.geeksforgeeks.org/']
+
+
