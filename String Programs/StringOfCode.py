53f9e3387b5e504142b2d52957c3924d790caca0
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-21 16:16:15 +0530

    Execute the string of code

--- String Programs/StringOfCode.py
+++ String Programs/StringOfCode.py
@@ -1,1 +1,9 @@
 #Execute the string of code
+#Given a string containing python code, task is to execute it dynamically
+#for eg: I/P: "x = 5\ny = 10\nprint(x + y)"  and   O/P: 15
+
+
+#Using exec()
+#exec() function allows is to execute dynamically generated python code stored in a string
+code = "x = 5\ny = 10\nprint(x + y)"
+exec(code)
