8a3200251f6eeb4f62637f077e1319deb66196d4
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-22 17:01:42 +0530

    String slicing to rotate a string

--- String Programs/StringSlicingtoRotate.py
+++ String Programs/StringSlicingtoRotate.py
@@ -19,9 +19,54 @@ print("right rotation:", right)
 #this method doubles the string (s+s) so all possible rotations appear inside it. then the rotated version is obtained by slicing the extended string at the correct position.
 s = "HelloGoodMorningHowIsItGoing"
 d = 2
-ext = s+s
+ext = s+s     #doubling the string contains all rotations
 n = len(s)
-left = ext[d: d+n]
-right = ext[n-d: 2*n - d]
+left = ext[d: d+n]       #slice starting at d for n chars
+right = ext[n-d: 2*n - d]       #slice starting d chars before n for n chars
+print("Left Rotation:", left)
+print("Right Rotation:", right)
+
+
+
+#Rotation using collections.deque
+from collections import deque
+s = "HelloGoodMorningHowIsItGoing"
+d = 2
+dq = deque(s)       #converts the string into a deque so we can rotate efficiently
+
+#left rotation
+dq.rotate(-d)       #negative value rotates left; collect the left rotated string
+left = ''.join(dq)
+
+#right rotation
+dq.rotate(d)    #reverses the previous rotation
+dq.rotate(d)    #rotates right by d, collect the right rotated string
+right = ''.join(dq)    #converts the deque back to a string
+
+print("Left Rotation:", left)
+print("Right Rotation", right)
+
+
+
+
+#Using for loop
+s = "HelloGoodMorningHowIsItGoing"
+d = 2
+n = len(s)
+
+#Left Rotation
+left = ""
+for i in range(d, n):
+    left += s[i]
+for i in range(d):
+    left += s[i]
+
+#Right Rotation
+right = ""
+for i in range(n-d, n):
+    right += s[i]
+for i in range(n-1):
+    right += s[i]
+
 print("Left Rotation:", left)
 print("Right Rotation:", right)
