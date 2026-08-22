e0ee11f2934644377480531b9951993fe0eb26d1
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-22 15:54:20 +0530

    String slicing to rotate a string

---
+++ String Programs/StringSlicingtoRotate.py
@@ -0,0 +1,5 @@
+#String slicing to rotate a string
+#Given a string of length n, rotate the string either, left (anticlockwise) by d positions or right(clockwise) by d positions where 0 <= d <= n.
+#Input: "GeeksforGeeks", d=2
+# Output: Left Rotation: "eksforGeeksGe" 
+#         Right Rotation:  "ksGeeksforGee"
