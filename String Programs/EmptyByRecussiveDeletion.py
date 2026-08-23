07bd2207e7723fa3106ea1a97903ab9c7255f17b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-23 22:02:55 +0530

    Check if a string can become empty by recursive deletion using slicing

--- String Programs/EmptyByRecussiveDeletion.py
+++ String Programs/EmptyByRecussiveDeletion.py
@@ -1,1 +1,3 @@
 #Check if a string can become empty by recursive deletion using slicing
+#Given a string S and a substring 'sub' the task is to determine whether 'S' can be reduced to an empty string by repeatedly deleting one occurence at a time
+#of the substring 'sub' whenever it appears in the string
