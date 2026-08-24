7a9edb970aa53de9f5d87b74cc4b75438a3c8d4b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-24 15:35:38 +0530

    Check if a string can become empty by recursive deletion using slicing

--- String Programs/EmptyByRecussiveDeletion.py
+++ String Programs/EmptyByRecussiveDeletion.py
@@ -1,6 +1,6 @@
 #Check if a string can become empty by recursive deletion using slicing
-#Given a string S and a substring 'sub' the task is to determine whether 'S' can be reduced to an empty string by repeatedly deleting one occurence at a time
-#of the substring 'sub' whenever it appears in the string
+#Given a string S and a substring 'sub' the task is to determine whether 'S' can be reduced to an empty string by repeatedly deleting one occurence at a time of the substring 'sub' whenever 
+# it appears in the string
 #Example:
 # Input:
 # s = "aaaaaa"
@@ -25,4 +25,25 @@ while True:
         break
     s = s[:ind] + s[ind + len(sub):]             #removes the found substring by keeping the part before it and after it
 result = (s == "")            #Checks whether the final strig is empty
+print(result)
+
+
+
+
+#Recursive deletion using slicing
+#recursive deletion using slicing means removing a substring from a string again and again by cutting it out using slicing (s[:]). The process keeps calling itself until either the string 
+# becomes empty or no more deletions are possible
+def recursiveDeletion(s, sub):
+    if not s:
+        return True
+    ind = s.find(sub)
+    
+    if ind != -1:
+        s1 = s[:ind] + s[ind + len(sub):]
+        return recursiveDeletion(s1, sub)
+    return False
+s = 'IamKirtikaIamHappyIamContentIamEnoughIamSupportive'
+sub = 'Iam'
+
+result = recursiveDeletion(s, sub)
 print(result)
