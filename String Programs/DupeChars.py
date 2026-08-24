b9ca711c4f695836507769079aa341575d618f58
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-24 16:38:42 +0530

    Find all duplicate chars in a string

---
+++ String Programs/DupeChars.py
@@ -0,0 +1,13 @@
+#Find all duplicate chars in a string
+#given a string, the task is to find all chars that appear more than once in it.
+# For eg: I/P - 'IamKirtikaPuniani'
+#         O/P - ['i', 'a', 'n']
+
+
+#Using collections.Counter
+# Counter() function automatically counts how many times each char appears. Then, we can easily extract chars that occur more than once
+from collections import Counter
+string = 'IamKirtikaPuniani'
+d = Counter(string)
+result = [i for i, count in d.items() if count > 1]
+print(result)
