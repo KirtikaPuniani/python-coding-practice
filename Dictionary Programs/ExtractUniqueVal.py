f55d2cea0a53dd1c6ba0aeb5ab8905a696a8a1a8
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-29 14:36:30 +0530

    Extract unique values dictionary values

--- Dictionary Programs/ExtractUniqueVal.py
+++ Dictionary Programs/ExtractUniqueVal.py
@@ -20,5 +20,5 @@ d = {'gfg' : [5,6,7,8],
      'is' : [10,11,7,5], 
      'best' : [6,12,10,8], 
      'for' : [1,2,5]}
-result = list(sorted({ele for val in d.values() for ele in val}))
+result = list(sorted({ele for val in d.values() for ele in val}))       #nested set comprehension flattens and removes duplicates. sorted returns a sorted list of unique values
 print(result)
