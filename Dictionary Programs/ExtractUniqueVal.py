105f6c9a3b48f9a13f422af5db4f316e2e725b40
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-29 14:35:33 +0530

    Extract unique values dictionary values

--- Dictionary Programs/ExtractUniqueVal.py
+++ Dictionary Programs/ExtractUniqueVal.py
@@ -1,7 +1,6 @@
 #Extract unique values dictionary values
 
 
-
 #Using set and sum
 d = {'gfg' : [5,6,7,8],
      'is' : [10,11,7,5], 
@@ -12,3 +11,14 @@ res = list(set(sum(d.values(), [])))      #data.values(): fetches all value list
 #set(): removes duplicate elements.
 #list(): converts the set into a list.
 print(res)
+
+
+
+
+#Using set comprehension + values + sorted
+d = {'gfg' : [5,6,7,8],
+     'is' : [10,11,7,5], 
+     'best' : [6,12,10,8], 
+     'for' : [1,2,5]}
+result = list(sorted({ele for val in d.values() for ele in val}))
+print(result)
