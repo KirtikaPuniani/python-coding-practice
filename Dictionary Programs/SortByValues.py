c423d0d7f85cf4f3e58717954446e98823f61a84
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-31 00:31:06 +0530

    Ways to sort list of dictionaries by values

---
+++ Dictionary Programs/SortByValues.py
@@ -0,0 +1,11 @@
+#Ways to sort list of dictionaries by values
+
+
+#Sort by single key
+from operator import itemgetter
+dict = [{'name': 'alice', 'age': 27},
+        {'name': 'cherish', 'age': 21},
+        {'name': 'neha', 'age': 31},
+        {'name': 'tarun', 'age': 35}
+]
+print("sorted by age: ", sorted(dict, key = itemgetter('age')))
