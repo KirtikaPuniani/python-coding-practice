aad4bd188ea2f6776f2e3588d218c14827eb0367
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-08-31 14:10:11 +0530

    sort list of dictionaries by values using lambda

--- Dictionary Programs/SortUsingLambda.py
+++ Dictionary Programs/SortUsingLambda.py
@@ -9,3 +9,14 @@ dict = [{'name': 'alice', 'age': 27},
 ]
 print("Sorted by age:")
 print(sorted(dict, key = lambda x: x['age']))
+
+
+
+#Sort by multiple keys
+dict = [{'name': 'alice', 'age': 27},
+        {'name': 'cherish', 'age': 21},
+        {'name': 'neha', 'age': 31},
+        {'name': 'tarun', 'age': 35}
+]
+print("\n Sorted by age and name:")
+print(sorted(dict, key = lambda x: (x['age'], x['name'])))
