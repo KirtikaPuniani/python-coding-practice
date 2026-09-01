8d48083fdcfa5dd9ce226ed822caa7f8279ed88b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-01 23:23:56 +0530

    Convert Key-Values List to Flat Dictionary

--- Dictionary Programs/ConvertKeys.py
+++ Dictionary Programs/ConvertKeys.py
@@ -1,2 +1,8 @@
 # Convert Key-Values List to Flat Dictionary
 
+#Use dict()
+a = [('name', 'Kirtika Puniani'), 
+     ('age', 27), 
+     ('profession', 'Python Developer'),
+     ('status', 'unemployed')]
+print(dict(a))
