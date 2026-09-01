a2c8be30cbca7b77ee8c10bf32f53b27001b3841
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-01 13:46:21 +0530

    Merging or concatenating 2 dictionaries

--- Dictionary Programs/MergeDict.py
+++ Dictionary Programs/MergeDict.py
@@ -22,3 +22,13 @@ dict1 = {'x' : 123, 'y' : 456}
 dict2 = {'a' : 789, 'b' : 345}
 dict1.update(dict2)        #adds all key value pairs from dict2 to dict1. If a key exists in both dict1 and dict2 the value from dict2 replaces the value in dict1
 print(dict1)
+
+
+
+#Use loop
+dict1 = {'x' : 123, 'y' : 456}
+dict2 = {'a' : 789, 'b' : 345}
+x = dict1.copy()       #creates a shallow copy of dict1 to preseve the original dictionary.
+for key, value in dict2.items():      #for loop iterates through each key value pair in dict2 and adds or updates in x
+    x[key] = value
+print(x)
