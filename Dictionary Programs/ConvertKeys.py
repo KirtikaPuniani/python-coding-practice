c87ec6c57e6342edc63ca5838a7180b119c488db
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-01 23:31:32 +0530

    Convert Key-Values List to Flat Dictionary

--- Dictionary Programs/ConvertKeys.py
+++ Dictionary Programs/ConvertKeys.py
@@ -6,3 +6,31 @@ a = [('name', 'Kirtika Puniani'),
      ('profession', 'Python Developer'),
      ('status', 'unemployed')]
 print(dict(a))
+
+
+#Using dictionary comprehension
+a = [('name', 'Kirtika Puniani'), 
+     ('age', 27), 
+     ('profession', 'Python Developer'),
+     ('status', 'unemployed')]
+print({key: value for key, value in a})
+
+
+
+#Use for loop
+a = [('name', 'Kirtika Puniani'), 
+     ('age', 27), 
+     ('profession', 'Python Developer'),
+     ('status', 'unemployed')]
+x = {}
+for key, value in a:
+    x[key] = value
+print(x)
+
+
+
+#Using zip
+a = ['name', 'age', 'profession', 'status']
+b = ['Kirtika Puniani', 27, 'Pyhton Developer', 'Unemployed']
+x = dict(zip(a,b))
+print(x)
