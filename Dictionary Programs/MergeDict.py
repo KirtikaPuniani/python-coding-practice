0b24061ba6ae702d8eaad65ad94868fdd9f72874
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-01 13:38:31 +0530

    Merging or concatenating 2 dictionaries

--- Dictionary Programs/MergeDict.py
+++ Dictionary Programs/MergeDict.py
@@ -4,5 +4,5 @@
 #Using | Operator
 dict1 = {'x' : 123, 'y' : 456}
 dict2 = {'a' : 789, 'b' : 345}
-x = dict1 | dict2
+x = dict1 | dict2         #| operator combines two dictionaries into a new dictionary. Duplicate keys are handeled by keeping the value from the dictionary on the right
 print(x)
