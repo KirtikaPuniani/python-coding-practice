bbbea62e9cf471ca7fbd0088835e342abb5db0a4
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-03 15:02:52 +0530

    Check order of character in string

--- Dictionary Programs/CheckOrder.py
+++ Dictionary Programs/CheckOrder.py
@@ -1,1 +1,20 @@
 #Check order of character in string using OrderedDict()
+
+
+#Using orderedDict() with index mapping
+from collections import OrderedDict
+string = 'Engineers Rock'
+x = 'er'
+
+od = OrderedDict()
+for i, ch in enumerate(string):
+    if ch not in od:
+        od[ch] = i
+position = -1
+for ch in x:
+    if ch not in od or od[ch] < position:
+        print(False)
+        break
+    position = od[ch]
+else:
+    print(True)
