eacd294540e34ac34c75154eae7b2cbe6bb3c847
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-04 17:56:40 +0530

    Append Dictionary Keys and Values in order

--- Dictionary Programs/AppendDict.py
+++ Dictionary Programs/AppendDict.py
@@ -4,5 +4,5 @@
 #Using zip and dictionary constructor
 keys = ['name', 'age', 'profession', 'status']
 values = ['Alice', 30, 'Engineer', 'Unemployed']
-dict1 = dict(zip(keys, values))
+dict1 = dict(zip(keys, values))       #zip(keys, values) - Pairs keys with their corresponding values and dict() creates the dictionary in insertion order
 print(dict1)
