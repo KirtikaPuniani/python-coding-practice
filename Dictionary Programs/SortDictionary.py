22717a94dc407ac4256cf94cca9db6f874b556b0
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-05 13:34:19 +0530

    Sort Python Dictionary by key or value

--- Dictionary Programs/SortDictionary.py
+++ Dictionary Programs/SortDictionary.py
@@ -5,3 +5,11 @@
 dict = {'watermelon': 10, 'apple': 5, 'banana': 7, 'grapes': 3}
 asc = {k: v for k, v in sorted(dict.items(), key = lambda item: item[1])}
 print(asc)          #Sorts the dictionary by its values in ascending order using sorted() with a lambda function as the key, and constructs a new dictionary with the sorted items.
+
+
+#Using OrderedDict
+from collections import OrderedDict
+dict = {'watermelon': 10, 'apple': 5, 'banana': 7, 'grapes': 3}
+asc = OrderedDict(sorted(dict.items(), key = lambda item: item[1]))          #Sorts the dictionary by its values in ascending order using
+#sorted() with a lambda function as the key, and constructs an OrderedDict to maintain the sorted order.
+print(asc)
