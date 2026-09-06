f124d4c61d660c39e91c33e8e4d72f943f38b604
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-06 11:55:33 +0530

    Handling missing keys in dictionaries

---
+++ Dictionary Programs/HandleMissingKeys.py
@@ -0,0 +1,9 @@
+#Handling missing keys in dictionaries
+
+#Using defaultdict
+from collections import defaultdict
+dict = defaultdict(lambda: 'Key not found')          #Creates a defaultdict with a default value for missing keys.
+dict['name'] = 'Alice'
+dict['age'] = 27
+print(dict['name'])          #Accesses an existing key, returns its value.
+print(dict['profession'])          #Accesses a missing key, returns the default value 'Key not found'.
