b5857ef46c2be36cf09d55003a119603a0e5928e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-06 12:05:50 +0530

    Handling missing keys in dictionaries

--- Dictionary Programs/HandleMissingKeys.py
+++ Dictionary Programs/HandleMissingKeys.py
@@ -18,3 +18,10 @@ print(dict.get('salary', 'Key not found'))          #Accesses a missing key, ret
 
 
 
+#Using setdefault method
+dict = {'name': 'Alice', 'age': 27, 'profession': 'Python Developer', 'status': 'unemployed'}
+print(dict.setdefault('name'))          #Accesses an existing key, returns its value.
+print(dict.setdefault('profession'))          #Accesses a missing key, returns None (default) or a specified default value.
+print(dict.setdefault('salary', 'Key not found'))          #Accesses a missing key, returns the specified default value 'Key not found' and adds the key-value pair to the dictionary.
+print(dict['name'])      #Accesses the existing key 'name', returns its value.
+print(dict['salary'])      #Accesses the newly added key 'salary', returns its value 'Key not found'.    #Prints the updated dictionary with the new key-value pair added for the missing key 'salary'.
