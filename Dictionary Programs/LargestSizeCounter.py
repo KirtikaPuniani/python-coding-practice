e7d155284e15409de4329c4e6a12e69800c1fdff
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-10 12:52:18 +0530

    Counter to find size of largest subset of anagram

--- Dictionary Programs/LargestSizeCounter.py
+++ Dictionary Programs/LargestSizeCounter.py
@@ -1,1 +1,3 @@
-#Counter to find the size of largest subset of anagram words
+#Counter to find the size of largest subset of anagram words.  --Counter to find size of largest subset of anagram
+# Given an array of strings containing lowercase letters, the task is to find the size of the largest subset of words that are anagrams of each other. Two strings are said to be anagrams if they contain the same characters, 
+# only in a different order.
