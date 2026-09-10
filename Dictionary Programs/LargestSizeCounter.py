aab45143946a27963b6a93769e6c0957b66d096d
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-11 00:00:29 +0530

    Counter to find size of largest subset of anagram

--- Dictionary Programs/LargestSizeCounter.py
+++ Dictionary Programs/LargestSizeCounter.py
@@ -1,3 +1,19 @@
 #Counter to find the size of largest subset of anagram words.  --Counter to find size of largest subset of anagram
 # Given an array of strings containing lowercase letters, the task is to find the size of the largest subset of words that are anagrams of each other. Two strings are said to be anagrams if they contain the same characters, 
 # only in a different order.
+
+
+#Using collections.Counter
+from collections import Counter
+def MaxAnagrams(input_string):
+    words = input_string.split(" ")
+    
+    for i in range(len(words)):
+        words[i] = ''.join(sorted(words[i]))
+    
+    frequencyDict = Counter(words)
+    print(max(frequencyDict.values()))
+
+if __name__ == "__main__":
+    input_string = 'ant magenta magnate tan gnamate'
+    MaxAnagrams(input_string)
