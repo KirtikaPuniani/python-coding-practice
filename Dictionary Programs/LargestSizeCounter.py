8d3bcffc72fda6934cffd4e8eb98d97652870505
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-12 16:26:47 +0530

    Counter to find size of largest subset of anagram

--- Dictionary Programs/LargestSizeCounter.py
+++ Dictionary Programs/LargestSizeCounter.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
 def largest_anagram_subset_size(words):
     anagram_dict = {}
     for word in words:
-        sorted_word = ''.join(sorted(word))
+        sorted_word = ''.join(sorted(word))     #sorts each word to create a common key for its anagrams.
         if sorted_word not in anagram_dict:
             anagram_dict[sorted_word] = []
-        anagram_dict[sorted_word].append(word)
+        anagram_dict[sorted_word].append(word)     #groups words that share the same sorted key.
     
-    return max(len(val) for val in anagram_dict.values())
+    return max(len(val) for val in anagram_dict.values())    #finds the size of the largest anagram group.
 
 words = ['ant', 'magenta', 'magnate', 'tan', 'gnamate']
 print(largest_anagram_subset_size(words))
