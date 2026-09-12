46db8cd92234b8f318eeb7b8d18a5e0ef4a7360c
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-12 16:28:37 +0530

    Counter to find size of largest subset of anagram

--- Dictionary Programs/LargestSizeCounter.py
+++ Dictionary Programs/LargestSizeCounter.py
@@ -41,6 +41,3 @@ from collections import Counter
 words = ['cars', 'bikes', 'arcs', 'steer']
 max_anagrams = max(map(lambda x: sum(Counter(y) == Counter(x) for y in words), words), default=0)       #Counter(y) == Counter(x): checks if two words are anagrams. sum(): counts how many anagrams each word has. map(): applies this to all words, and max() returns the largest count.
 print(max_anagrams)
-
-
-
