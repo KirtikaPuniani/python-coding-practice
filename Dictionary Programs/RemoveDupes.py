a1d791f24a5fb9363cc65f3168b41ae35f1090d5
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-12 18:59:25 +0530

    Remove all duplicate words from a given sentence

--- Dictionary Programs/RemoveDupes.py
+++ Dictionary Programs/RemoveDupes.py
@@ -3,7 +3,17 @@
 
 #Using dict.fromkeys
 string = '''my name is kirtika puniani. my brother's name is cherish'''
-str = string.split()
-string_op = list(dict.fromkeys(str))
-result = ' '.join(string_op)
+str = string.split()       #string.split splits the sentence into words
+string_op = list(dict.fromkeys(str))       #dict.fromkeys creates keys from words, automatically removing duplicates while maintaining their first occurence. list function converts dictionary keys back to a list
+result = ' '.join(string_op)     #join function joins list elements back into a string separated by spaces
 print(result)
+
+
+
+#Using list comprehension with set
+string = '''my name is kirtika puniani. my brother's name is cherish'''
+str = string.split()
+seen = set()
+string_op = [word for word in str if not (word in seen or seen.add(word))]
+result = ' '. join(string_op)
+print(result) 
