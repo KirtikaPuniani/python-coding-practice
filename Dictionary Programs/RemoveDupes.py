c8cd62c7b915291d9ab3eb3535ccef3229208703
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-13 18:54:41 +0530

    Remove all duplicate words from a given sentence

--- Dictionary Programs/RemoveDupes.py
+++ Dictionary Programs/RemoveDupes.py
@@ -13,7 +13,7 @@ print(result)
 #Using list comprehension with set
 string = '''my name is kirtika puniani. my brother's name is cherish'''
 str = string.split()
-seen = set()
+seen = set()       #set(), empty set created to record words already seen
 string_op = [word for word in str if not (word in seen or seen.add(word))]
 result = ' '. join(string_op)
 print(result) 
