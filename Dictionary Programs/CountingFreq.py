0d4c639255d2c2d0b781f8ad6f57f193ead05722
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-14 17:03:21 +0530

    Counting frequencies in a list using dictionary

--- Dictionary Programs/CountingFreq.py
+++ Dictionary Programs/CountingFreq.py
@@ -1,1 +1,8 @@
 #Counting frequencies in a list using dictionary
+
+
+#Using Counter from collections
+from collections import Counter
+a = ['apple', 'banana', 'kiwi', 'apple', 'grapes', 'banana', 'orange']
+frequency = Counter(a)
+print(dict(frequency))
