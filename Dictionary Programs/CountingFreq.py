220d024f0591eadcc809b20d4da28e3c88ba626c
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-15 16:50:09 +0530

    Counting frequencies in a list using dictionary

--- Dictionary Programs/CountingFreq.py
+++ Dictionary Programs/CountingFreq.py
@@ -16,3 +16,24 @@ frequency = defaultdict(int)         #defaultdict(int) creates a dictionary wher
 for item in a:        #for reach item in a, frequency[item] += 1 increments its count
     frequency[item] += 1
 print(dict(frequency))
+
+
+
+#Using get method
+a = ['apple', 'banana', 'kiwi', 'apple', 'grapes', 'banana', 'orange']
+frequency = {}
+for item in a:
+    frequency[item] = frequency.get(item, 0) + 1        #frequency.get(item, 0) returns the current count of item or 0 if it doesn't exist and adding 1 increments the count
+print(frequency)
+
+
+
+#Using dictionary
+a = ['apple', 'banana', 'kiwi', 'apple', 'grapes', 'banana', 'orange']
+frequency = {}
+for item in a:
+    if item in frequency:
+        frequency[item] += 1
+    else:
+        frequency[item] = 1
+print(frequency)
