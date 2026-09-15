71c46b8606433884fdb9c85bb42197bcfa7f9142
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-15 22:08:38 +0530

    Convert a list of tuples into dictionary

---
+++ Dictionary Programs/TupleToDict.py
@@ -0,0 +1,16 @@
+#Convert a list of tuples into dictionary
+
+
+#Using dict()
+a = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+result = dict(a)
+print(result)
+
+
+#Using for loop
+a = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+result = {}
+for key, value in a:
+    result[key] = value
+print(result)
+    
