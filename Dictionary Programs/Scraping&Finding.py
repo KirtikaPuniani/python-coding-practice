bf80ba5a80aacd4970482f79abdf6725d5a08efc
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-16 16:33:27 +0530

    Scraping and Finding ordered words in a dictionary

--- Dictionary Programs/Scraping&Finding.py
+++ Dictionary Programs/Scraping&Finding.py
@@ -1,3 +1,14 @@
 #Scraping and Finding ordered words in a dictionary
 #An ordered word is a word in which the letters appear in alphabetical order. eg: abbey -> ordered and geeks -> not ordered
 
+
+import requests 
+url = 'https://www.puzzlers.org/pub/wordlists/unixdict.txt'
+fd = requests.get(url)
+c1 = fd.content.decode('utf-8').split()[16:]
+
+for word in c1:
+    if len(word) < 3:
+        continue
+    if all(ord(word[i]) <= ord(word[i+1]) for i in range(len(word)-1)):
+        print(f"{word}: Word is ordered")
