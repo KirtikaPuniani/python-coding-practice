f0956d896e746151cf1354d6918198546aa5f6d0
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-17 13:38:43 +0530

    Possible words using given chars

--- Dictionary Programs/PossibleWords.py
+++ Dictionary Programs/PossibleWords.py
@@ -1,2 +1,10 @@
 #Possible words using given chars
 #Given a list of owrds and a list of chars, print all valid words that can be formed using those chars. Repetetion of chars is not allowed.
+
+
+#Using set and subsets
+dict = ["go", "bat", "me", "eat", "goal", "boy", "run"]
+ch = ['e', 'o', 'b', 'a', 'm', 'g', 'l']
+x = set(ch)
+res = [w for w in dict if set(w).issubset(x)]
+print(res)
