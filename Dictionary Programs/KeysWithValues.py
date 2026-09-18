59c12e3b70809deaa034ebdb336f1334e809bf5b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-19 01:22:59 +0530

    Keys associated with values in dictionary

--- Dictionary Programs/KeysWithValues.py
+++ Dictionary Programs/KeysWithValues.py
@@ -1,1 +1,7 @@
 #Keys associated with values in dictionary
+#Given a dictionary in Python, our task is to reverse the mapping, i.e., create a new dictionary where each value points to the keys it belongs to. This operation is useful in data organization, analytics, and web applications.
+# Input: {'abc': [10, 30], 'bcd': [30, 40, 10]}
+# Output: {10: ['abc', 'bcd'], 30: ['abc', 'bcd'], 40: ['bcd']}
+
+
+
