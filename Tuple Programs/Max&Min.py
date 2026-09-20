9eae1da4f81946a4df405e5be5bf521b15a0a8b6
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-21 00:49:31 +0530

    Maximum and minimum k elements

--- Tuple Programs/Max&Min.py
+++ Tuple Programs/Max&Min.py
@@ -10,3 +10,15 @@ l = heapq.nlargest(k, a)       #Returns the k largest elements from the iterable
 
 print(s)
 print(l)
+
+
+
+#Using list slicing and sorted
+#Convert tuple to list, sort it, and slice the first and last K elements
+a = (1, 3, 45, 5, 77, 45, 66, 23)
+k = 1
+temp = sorted(a)
+min = temp[:k]
+max = temp[-k:]
+print(min)
+print(max)
