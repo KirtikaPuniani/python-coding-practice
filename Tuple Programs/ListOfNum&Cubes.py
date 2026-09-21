3213e1f9ead4f0d3cb09ed6dd4661587dcde1b79
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-22 00:39:58 +0530

    List of tuples with numbers and their cubes

--- Tuple Programs/ListOfNum&Cubes.py
+++ Tuple Programs/ListOfNum&Cubes.py
@@ -1,1 +1,4 @@
 #Create a list of tuples with numbers and their cubes
+#Given a list of numbers, the task is to create a list of tuples where each tuple contains a number and its cube.
+# Input: [1, 2, 3]
+# Output: [(1, 1), (2, 8), (3, 27)]
