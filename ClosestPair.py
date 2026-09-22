b2e20f1f6aca23419fd419b054362f162d00841b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-23 00:21:49 +0530

    Closest pair to kth index element in tuple

--- ClosestPair.py
+++ ClosestPair.py
@@ -9,3 +9,15 @@ y = (8,10)
 k = 2
 output = min(x, key = lambda x: abs(x[k-1] - y[k-1]))       #computes the absolute difference for each tuple and returns the one with the smallest difference
 print(output)
+
+
+#Using emunerate and loop
+x = [(1,3,5), (2,4,6), (7,9,23)]
+y = (8,10,12)
+k = 3
+min_diff, output = float('inf'), None
+for index, value in enumerate(x):
+    diff = abs(value[k-1] - y[k-1])
+    if diff < min_diff:
+        min_diff, output = diff, index
+print(x[output])
