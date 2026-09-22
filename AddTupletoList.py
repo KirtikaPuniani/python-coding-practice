d6afa5c2018aaca561b2258b9b616039f07ba6c2
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-22 15:30:37 +0530

    Adding tuple to list and vice-versa

---
+++ AddTupletoList.py
@@ -0,0 +1,10 @@
+#Adding tuple to list and vice-versa
+
+
+#Using extend and '+' operator
+a = [1,2,3]
+b= (4,5)
+# a.extend(b)      #add tuple to list 'a'. adds each element of b to a.
+# print(a)
+x = tuple(a) + b     #add list to tuple 'b'. we can't do simple a + b because the type of variables is diff, one is tuple and other is list so we have to type cast the variable.
+print(x)
