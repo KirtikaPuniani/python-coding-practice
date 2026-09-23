1fa6a40adf5df2322476fd4a301e8520bab46eb1
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-24 01:40:36 +0530

    join tuples if similar initial element

--- JoinTuples.py
+++ JoinTuples.py
@@ -3,3 +3,15 @@
 #Example:
 # Input: [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
 # Output: [(5, 6, 7, 8), (6, 10), (7, 13)] 
+
+
+
+
+#Usingdefaultdict and loop
+from collections import defaultdict
+tup = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
+map = defaultdict(list)
+for k, v in tup:
+    map[k].append(v)
+res = [(k, *v) for k, v in map.items()]
+print(res)
