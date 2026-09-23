fdce380d7a8bbf4f23f7d562be522f2dbc5ace30
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-24 01:37:59 +0530

    join tuples if similar initial element

---
+++ JoinTuples.py
@@ -0,0 +1,5 @@
+#join tuples if similar initial element
+#Given a list of tuples, combine (or join) all tuples that share the same first element into a single tuple. The order of remaining elements should be preserved.
+#Example:
+# Input: [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
+# Output: [(5, 6, 7, 8), (6, 10), (7, 13)] 
