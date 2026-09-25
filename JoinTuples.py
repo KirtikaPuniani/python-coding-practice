70c0507f3c4c5977a75fd30478df382b37d4df6b
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-25 13:48:11 +0530

    Join tuples if similar initial element

--- JoinTuples.py
+++ JoinTuples.py
@@ -1,4 +1,4 @@
-#join tuples if similar initial element
+#Join tuples if similar initial element
 #Given a list of tuples, combine (or join) all tuples that share the same first element into a single tuple. The order of remaining elements should be preserved.
 #Example:
 # Input: [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
@@ -7,7 +7,7 @@
 
 
 
-#Usingdefaultdict and loop
+#Using defaultdict and loop
 from collections import defaultdict
 tup = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
 map = defaultdict(list)
@@ -15,3 +15,7 @@ for k, v in tup:
     map[k].append(v)
 res = [(k, *v) for k, v in map.items()]
 print(res)
+
+
+
+#Using itertools.groupby()
