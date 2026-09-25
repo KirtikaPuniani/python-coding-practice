e9899f6af97da1e947b3f8c71febc2265b8769c8
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-09-25 14:13:17 +0530

    Join tuples if similar initial element

--- JoinTuples.py
+++ JoinTuples.py
@@ -36,3 +36,34 @@ for x in tup:
     temp[x[0]] = temp.get(x[0], []) + list(x[1:])         #temp.get() function Returns current values or initializes an empty list. ## + list(x[1:]): Adds tuple elements to the existing list ### (k,) + tuple(v): Combines key and grouped values.
 output = [(k,) + tuple(v) for k, v in temp.items()]
 print(output)
+
+
+
+#Using recursion
+def join_tup(tup, i):
+    if i == len(tup) - 1:
+        return tup
+    elif tup[i][0] == tup[i+1][0]:           
+        tup[i] += tup[i+1][1:]
+        tup.pop(i+1)
+        return join_tup(tup, i)
+    else:
+        return join_tup(tup, i+1)
+tup = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
+output = join_tup(tup, 0)
+print(output)
+
+
+
+def join_tup(tup, i):
+    if i == len(tup) - 1:
+        return tup
+    elif tup[i][0] == tup[i+1][0]:           #Checks if consecutive tuples share the same key
+        tup[i] += tup[i+1][1:]           #Merges next tuple’s values
+        tup.pop(i+1)        #removes the merged tuple
+        return join_tup(tup, i)
+    else:
+        return join_tup(tup, i+1)
+tup = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)] 
+output = join_tup(tup, 0)
+print(output)
